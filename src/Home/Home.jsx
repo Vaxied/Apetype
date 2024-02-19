76c5b668518674ddd8d2f1b9a9e301ce7a217514
Francisco Javier Lopez Tirado <wkm.jtirado@terceiros.mag.com.br>
2024-02-19 02:06:16 -0300

    create router and componente for home and profile

---
+++ src/Home/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+      <div>
+        <h1>This is the home page</h1>
+      </div>
+    );
+  }
+   export default Home;
---
+++ src/Profile/Profile.jsx
@@ -0,0 +1,8 @@
+function Profile() {
+    return (
+      <div>
+        <h1>This is the Profile page</h1>
+      </div>
+    );
+  }
+   export default Profile;
